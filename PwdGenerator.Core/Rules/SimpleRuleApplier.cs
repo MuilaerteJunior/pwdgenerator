d35a632fd30b0befde2ca0eb04ff8c55a90708f5
Muilaerte Junior <mfvasjr@gmail.com>
2025-10-28 19:00:13 -0300

    #2 Changing UI to a more flexible use

--- PwdGenerator.Core/Rules/SimpleRuleApplier.cs
+++ PwdGenerator.Core/Rules/SimpleRuleApplier.cs
@@ -2,24 +2,23 @@
 {
     public abstract class SimpleRuleApplier
     {
-        protected static Random random = Random.Shared;
+        protected static readonly Random _random = Random.Shared;
         protected abstract Stack<char> GenerateContent(short ruleCount);
-        protected IEnumerable<int> GetIndexes(int length, short ruleCount)
+        protected static IEnumerable<int> GetIndexes(int length, short ruleCount)
         {
             if (ruleCount > length) throw new NotSupportedException("RuleCount > length");
-            if (ruleCount == length) return Enumerable.Range(0, length).ToArray();
+            if (ruleCount == length) return [.. Enumerable.Range(0, length)];
 
             var finalIndexes = new HashSet<int>();
             while (finalIndexes.Count < ruleCount)
             {
-                var index = random.Next(length);
-                if (!finalIndexes.Contains(index))
-                    finalIndexes.Add(index);
+                var index = _random.Next(length);
+                finalIndexes.Add(index);
             }
             return finalIndexes;
         }
 
-        protected string ReplaceCharAt(string input, IEnumerable<int> indexes, Stack<char> newChar)
+        protected static string ReplaceCharAt(string input, IEnumerable<int> indexes, Stack<char> newChar)
         {
             if (indexes == null || !indexes.Any())
                 return input;
@@ -27,12 +26,12 @@
                 throw new ArgumentOutOfRangeException(nameof(indexes), "Index is out of range.");
 
             var charArray = input.ToCharArray();
-            foreach (var index in indexes)
+            foreach (var index in indexes!)
                 charArray[index] = newChar.Pop();
             return new string(charArray);
         }
 
-        protected string UppercaseCharsAt(string input, IEnumerable<int> indexes)
+        protected static string UppercaseCharsAt(string input, IEnumerable<int> indexes)
         {
             if (indexes == null || !indexes.Any())
                 return input;
@@ -40,7 +39,7 @@
                 throw new ArgumentOutOfRangeException(nameof(indexes), "Index is out of range.");
 
             var charArray = input.ToCharArray();
-            foreach (var index in indexes)
+            foreach (var index in indexes!)
                 charArray[index] = char.ToUpper(charArray[index]);
             return new string(charArray);
         }
